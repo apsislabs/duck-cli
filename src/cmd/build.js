c7386f29a5c6d5198ed5dfa4addd8a1aafa131be
Wyatt Kirby <wyatt@apsis.io>
2018-10-24 12:32:52 -0700

    Reorganize build code

--- src/cmd/build.js
+++ src/cmd/build.js
@@ -1,10 +1,33 @@
+import { resolve, join } from "path";
 import chalk from "chalk";
+import { existsSync } from "fs";
 import { logBuildHelp } from "./helps";
+import { printAndExit } from "../lib/utils/logger";
+
+const REQUIRED_SUBDIRS = ["data", "templates", "deck.config.yml"];
 
 export const Build = args => {
   if (args.help) {
     logBuildHelp();
   }
 
-  console.log(chalk.green("Building!"));
+  // Start
+  const dir = resolve(args.path);
+  console.log(chalk.green(`Building decks in ${dir}`));
+
+  // Check Dependencies
+  if (!existsSync(dir)) {
+    printAndExit(chalk.red(`No such directory ${dir}.`));
+  }
+
+  REQUIRED_SUBDIRS.forEach(subdir => {
+    if (!existsSync(join(dir, subdir))) {
+      printAndExit(chalk.red(`Couldn\'t find a ${subdir} directory.`));
+    }
+  });
+
+  // Build
+
+  // Done
+  printAndExit(chalk.green("Build complete!"));
 };
