db9b331cfbb57e0b3abe28bc5b837ebddf1d0b6b
Wyatt Kirby <wyatt@apsis.io>
2018-10-24 11:57:24 -0700

    Add help logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import pkg from "../package.json";
 import parseArgs from "minimist";
-
+import _ from "lodash";
 import { Init } from "./cmd/init.js";
 import { Build } from "./cmd/build.js";
 import chalk from "chalk";
@@ -14,10 +14,25 @@ const argv = parseArgs(process.argv.slice(2));
 const defaultCmd = "build";
 const cmd = argv._[0];
 
-if (cmd) {
+const logHelp = () => {
+  console.log(`
+    Usage:
+      $ duck <command>
+    Available commands:
+      ${_.keys(commands).join(", ")}
+    For more information run a command with the --help flag
+      $ duck <command> --help
+  `);
+
+  process.exit(0);
+};
+
+if (!cmd) {
+  commands[defaultCmd]();
+} else if (cmd && _.has(commands, cmd)) {
   commands[cmd]();
 } else {
-  commands[defaultCmd]();
+  logHelp();
 }
 
 console.log(chalk.green("Done!"));
