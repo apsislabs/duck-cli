aea45c28df66b47b1a30c8a813b0b85acc6aee1f
Wyatt Kirby <wyatt@apsis.io>
2018-10-24 17:33:10 -0700

    Update template to use component library

--- test/templates/deck_one.js
+++ test/templates/deck_one.js
@@ -1,7 +1,18 @@
+import { Card, Circle, Group, RichText } from "duck-cli";
 import React from "react";
-import moment from 'moment';
+
 export default ({ color }) => (
-  <g>
-    <circle cx={50} cy={50} r={10} time={moment().toISOString()} fill={color} />
-  </g>
+  <Card>
+    <Group>
+      <Circle cx={425} cy={125} r={150} fill={color} clipPath="url(#safe)" />
+
+      <RichText x={75} y={300} width="700" height="400">
+        In publishing and graphic design, lorem ipsum is a placeholder text used
+        to demonstrate the visual form of a <b>document</b> without relying on
+        meaningful content. Replacing the actual content with placeholder text
+        allows designers to design the form of the content before the content
+        itself has been produced.
+      </RichText>
+    </Group>
+  </Card>
 );
