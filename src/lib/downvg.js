f40eb49d1d830e0e6f1ca5f1ff47b1c28afb17cb
Wyatt Kirby <wyatt@apsis.io>
2018-10-30 20:25:43 -0700

    Revert changes to downvg

--- src/lib/downvg.js
+++ src/lib/downvg.js
@@ -1,18 +1,15 @@
 const rules = [
   {
     regex: /(\*\*|__)(.*?)\1/g,
-    replacement:
-      "<tspan dominant-baseline='no-change' font-weight='bold'>$2</tspan>"
+    replacement: "<tspan font-weight='bold'>$2</tspan>"
   },
   {
     regex: /(\*|_)(.*?)\1/g,
-    replacement:
-      "<tspandominantBaseline dominant-baseline='no-change' font-style='italic'>$2</tspan>"
+    replacement: "<tspan font-style='italic'>$2</tspan>"
   },
   {
     regex: /\~\~(.*?)\~\~/g,
-    replacement:
-      "<tspandominantBaseline dominant-baseline='no-change' text-decoration='line-through'>$1</tspan>"
+    replacement: "<tspan text-decoration='line-through'>$1</tspan>"
   }
 ];
 
