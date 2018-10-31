65ec5b5f2b6a16c06aef928aa6e47161bbe43ea6
Wyatt Kirby <wyatt@apsis.io>
2018-10-31 12:32:32 -0700

    Update transform to transpile recursively

--- src/bin/lib/utils/fsp.js
+++ src/bin/lib/utils/fsp.js
@@ -1,19 +1,31 @@
-import fs from "fs";
+import fs, { writeFile } from "fs";
 import path from "path";
+import mkdirp from "mkdirp";
 import { promisify } from "util";
 
-var readJson = (p, cb) => {
+const readJson = (p, cb) => {
   fs.readFile(path.resolve(p), (err, data) => {
     if (err) cb(err);
     else cb(null, JSON.parse(data));
   });
 };
 
+const writeWithDirs = (dest, content, cb) => {
+  const destDir = path.dirname(dest);
+  if (!fs.existsSync(destDir)) {
+    mkdirp.sync(destDir);
+  }
+
+  fs.writeFile(dest, content, cb);
+};
+
 export default {
+  mkdirp: promisify(mkdirp),
   readJson: promisify(readJson),
   exists: promisify(fs.exists),
   mkdir: promisify(fs.mkdir),
   writeFile: promisify(fs.writeFile),
+  writeWithDirs: promisify(writeWithDirs),
   readFile: promisify(fs.readFile),
   unlink: promisify(fs.unlink),
   rmdir: promisify(fs.rmdir)
