87fbb0ee7932880727020fe2484b14ff4a2fb21f
Wyatt Kirby <wyatt@apsis.io>
2018-11-02 13:40:43 -0700

    Refactor shared finalize code

--- src/bin/lib/format.js
+++ src/bin/lib/format.js
@@ -2,12 +2,12 @@ import { createConverter } from "convert-svg-to-png";
 import miss from "mississippi";
 import path from "path";
 import rimraf from "rimraf";
+import { capStream } from "./streams/capStream";
 import { cropStream } from "./streams/cropStream";
 import { pdfStream } from "./streams/pdfStream";
 import { pngStream } from "./streams/pngStream";
 import { svgStream } from "./streams/svgStream";
 import fsp from "./utils/fsp";
-import { capStream } from "./streams/capStream";
 
 export const formatCards = async (projectRoot, config, data, renderings) => {
   for (const deckKey in config) {
@@ -30,6 +30,12 @@ const formatDeck = async (projectRoot, config, renderings, deckKey) => {
   const converter = createConverter();
   const size = renderings.length;
   let renderingStream = miss.from.obj(renderings);
+
+  const finalize = async err => {
+    if (err) rej(err);
+    if (converter) converter.destroy();
+    res();
+  };
 
   return await new Promise((res, rej) => {
     if (svg) {
@@ -59,12 +65,9 @@ const formatDeck = async (projectRoot, config, renderings, deckKey) => {
 
     renderingStream
       .pipe(capStream())
-      .on("error", rej)
-      .on("finish", async err => {
-        if (err) rej(err);
-        converter.destroy();
-        res();
-      });
+      .on("error", finalize)
+      .on("end", finalize)
+      .on("finish", finalize);
   });
 };
 
